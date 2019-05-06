f2b8598a9a7a36696602b522003ab9103aee22b2
James Spencer <thisjustosay@gmail.com>
2019-05-06 14:52:30 -0700

    make cards tappable, add debug styling

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,13 +1,24 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useCallback } from 'react'
 import { validChar } from '../util/utils';
 
 const style = {
-    base: {padding: '20px', width: '10vw', height:'10vh'},
+    base: {padding: '20px', width: '10vw', height:'10vh', border: '1px solid gray'},
     inactive: {backgroundColor: 'green'},
     active: {backgroundColor: 'yellow'}
 }
 
 type Props = { char: validChar}
 export const Card: FC<Props> = ({char}) => {
-    return (<div style={style.base}>{char}</div>)
+    const [isActive, setActive] = useState(false)
+    const toggleState = useCallback(() => {
+        setActive(!isActive)
+    }, [isActive])
+
+    return (<div onClick={toggleState}>
+        { isActive 
+            ? <div style={style.base}>{char}</div>
+            : <div style={style.base}></div>
+        }
+    </div>
+    )
 }
