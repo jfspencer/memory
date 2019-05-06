d9a573618fe0c52252843386144d3ae879cb9e63
James Spencer <thisjustosay@gmail.com>
2019-05-06 14:16:41 -0700

    finish implementing genGameBoard, passing tests

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,16 +1,12 @@
-import flatMap from 'lodash/fp/flatMap'
-import shuffle from 'lodash/fp/shuffle';
+import {flatMap, shuffle, chunk} from 'lodash/fp'
 
 type validChar = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H'
 type GameBoard = validChar[][]
 
-export const genGameBoard = (charArr: string[]):GameBoard => {
+export const genGameBoard = (charArr: validChar[]):GameBoard => {
     //flatmap the charArray to generate the char pairs
     const charPairs = flatMap(v => [v,v], charArr)
     const randomizedPairs = shuffle(charPairs)
-    
-
-
-
-    return [['A'],['A']]
+    console.log(chunk(4, randomizedPairs) as any)
+    return chunk(4, randomizedPairs) as GameBoard
 }
