8430de550ae38a01e31a77157ae118bffc809391
James Spencer <thisjustosay@gmail.com>
2019-05-07 02:06:13 -0700

    commit prod build to source control

---
+++ build/precache-manifest.4f88bfa4e4bed78e88f6f07bbb4dd665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb038a05f59335780090d31704cc0ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b2ec39177f1a739fc68",
+    "url": "/static/css/main.e164d7f5.chunk.css"
+  },
+  {
+    "revision": "4ed208d6063ed6e2cb94",
+    "url": "/static/js/2.bafe0b92.chunk.js"
+  },
+  {
+    "revision": "3b2ec39177f1a739fc68",
+    "url": "/static/js/main.8d31d6d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
