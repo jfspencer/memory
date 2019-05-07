63308306ee2d017ac0dceb72a677928a064dc137
James Spencer <thisjustosay@gmail.com>
2019-05-07 06:30:35 -0400

    fix prod hrefs

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -2,9 +2,10 @@ import {flatMap, shuffle, chunk} from 'lodash/fp'
 export type CardState = {char:string, found: boolean, id: number}
 export type GameConfig = CardState[][]
 
-const gameChars: string = "A,B,C,D,E,@,AB,BA"
+const defaultGameChars: string = "A,B,C,D,E,@,AB,BA"
 let count = 0
-export const initGameBoard = (col = 4, symbols = gameChars):GameConfig => {
+
+export const initGameBoard = (col = 4, symbols = defaultGameChars):GameConfig => {
     const charPairs = flatMap(v => [v,v], symbols.split(','))
     const randomizedPairs = shuffle(charPairs).map(char => ({char, found: false, id: genId() }))
     count = count + 1
