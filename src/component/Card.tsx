bb0a935c126812d5582c7c4ab5eab4be9662ec2f
James Spencer <thisjustosay@gmail.com>
2019-05-06 22:20:32 -0700

    card style animations

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -4,21 +4,21 @@ import { CardState } from '../util/utils';
 import { CardTap, CardClear } from '../state/SessionState';
 
 const style = {
-    base: {padding: '20px', width: '10vw', height:'10vh', border: '1px solid gray'},
+    base: {color:'black',padding: '20px', width: '10vw', height:'10vh', border: '1px solid gray', transition:'all 0.2s'},
     inactive: {backgroundColor: 'green'},
-    active: {backgroundColor: 'yellow'}
+    active: {backgroundColor: 'rgb(235,235,235)'}
 }
 
 type Props = { card: CardState, CardTap: any, CardClear: any, turn: CardState[]}
 const _Card: FC<Props> = ({card, CardTap, CardClear, turn}) => {
     const toggleState = useCallback(() => 
         !turn.find(v => v.id === card.id) ? CardTap(card) : CardClear(card), [turn])
-        
+
     const cardState = card.found 
-    ? (<div style={style.base}>{card.char}</div>) 
+    ? (<div style={{...{ transform: 'rotateY(0deg)'}, ...style.base, ...style.active}}>{card.char}</div>) 
     : !!turn.find(v => v.id === card.id)
-        ? <div style={style.base}>{card.char}</div>
-        : <div style={style.base}></div>
+        ? <div style={{...{ transform: 'rotateY(0deg)'}, ...style.base, ...style.active}}>{card.char}</div>
+        : <div style={{...{ transform: 'rotateY(-180deg)'}, ...style.base, ...style.inactive}}></div>
     return (<div onClick={toggleState}> {cardState}</div>
     )
 }
