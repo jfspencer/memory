fe7c158a1e15c191097f0c0a6f992b34b81cde73
James Spencer <thisjustosay@gmail.com>
2019-05-06 23:45:00 -0700

    fix off by one after match is found

--- src/state/SessionState.ts
+++ src/state/SessionState.ts
@@ -22,26 +22,24 @@ export function SessionStateReducer(state = initialState, action: {type: string,
       case '[Card] TAP':
         if(state.playerTurn.length  < 2) return {...state, playerTurn:[...state.playerTurn, action.payload]}
         else if(state.playerTurn.length === 2) {
-          console.log(state.playerTurn)
           const [c1, c2] = state.playerTurn
-
-          if(c1.char === c2.char){
+          if(c1.char === c2.char) {
             const flat = flatMap((v: CardState) => v, state.boardConfig)
             const parts = partition((v: CardState) => v.id === c1.id || v.id === c2.id, flat)
             const [[match1, match2]] = map(([v1, v2]: any) => [{...v1, found:true}, {...v2, found:true}], parts)
-            
-            const updatedBoard = map(row => row.map(card => {
-              if(card.id === match1.id) return  match1;
-              else if(card.id === match2.id) return match2;
-              else return card;
-            }), {...state.boardConfig})
-            return {...state, boardConfig: updatedBoard, playerTurn:[]}  
+            const updatedBoard = map(row => row.map(updateRowMatch(match1, match2)), {...state.boardConfig})
+            return {...state, boardConfig: updatedBoard, playerTurn:[action.payload]}  
           }
           return {...state, playerTurn:[action.payload]}
         }
-        
         return state
       default:
         return state
     }
+  }
+
+  const updateRowMatch = (m1: CardState, m2:CardState) => (card: CardState) =>  {
+    if(card.id === m1.id) return  m1;
+    else if(card.id === m2.id) return m2;
+    else return card;
   }
