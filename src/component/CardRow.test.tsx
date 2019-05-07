b64d4d4dd62ffbbb3294ea2cc1f02aad7f4b4048
James Spencer <thisjustosay@gmail.com>
2019-05-07 09:46:52 -0400

    cleanup

--- src/component/CardRow.test.tsx
+++ src/component/CardRow.test.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Card } from './Card';
 import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
+import { render, cleanup } from 'react-testing-library'
+afterEach(cleanup)
 
 describe('CardRow', () => {
     it('should map  over row prop and generate a list of Cards components', () => {
